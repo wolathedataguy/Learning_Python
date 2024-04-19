a501f9dd38a8447de7ef5e6548fd7d296ee34b3d
wolathedataguy <adekogs85@gmail.com>
2024-04-19 13:01:05 +0100

    Exercises

--- learning_c.py
+++ learning_c.py
@@ -1,4 +1,10 @@
 filepath = 'learning_python.txt'
 
 with open(filepath) as file_object:
-    line
+    lines = file_object.readlines()
+    for line in lines:
+        print(line.replace("Python","C"))
+
+with open(filepath) as file:
+    for line in file:
+        print(line.replace("Python","C"))
